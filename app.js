253b7a6c93c2a6d9c8508c3b064369a30552dba1
Zöld András <suti1979@Zold-MacBook-Air.local>
2021-03-15 20:48:56 +0100

    ledurts

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express")
 const rwoController = require("./controllers/rwo")
 const app = express()
 
-const HOST = "localhost"
+const HOST = "127.0.0.1"
 const PORT = "5000"
 
 app.set("view engine", "ejs")
-app.use(express.static("./public"))
+app.use(express.static(__dirname + "/public"))
 rwoController(app)
 
 app.listen(PORT, HOST, () => {
